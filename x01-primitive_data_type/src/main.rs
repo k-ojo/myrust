604f296a7d24f6cdc6cb5fb72287c6b4210deabb
kojo <gideonad4@gmail.com>
2025-10-01 09:04:42 +0000

    added files

---
+++ x01-primitive_data_type/src/main.rs
@@ -0,0 +1,20 @@
+//int and uint
+fn main(){
+    let x: i32 = 42;
+    let y: u64 = 100;
+    println!("x: {}, y: {}", x, y);
+
+    //Floats [floating point numbers]
+
+    let pi: f32 = 3.14;
+    let e: f64 = 2.718281828459045;
+    println!("pi: {}, e: {}", pi, e);
+
+    let is_active: bool = true;
+    let is_admin: bool = false;
+    println!("is_active: {}, is_admin: {}", is_active, is_admin);
+
+    let letter: char = 'A';
+    let emoji: char = '😊';
+    println!("letter: {}, emoji: {}", letter, emoji);
+}
